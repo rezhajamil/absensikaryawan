183272e5398ad846b1ca3e5467140e32ab5c70c8
Azisten <azistencom@gmail.com>
2022-10-04 12:13:01 +0700

    Add The Stage Warn

--- telkomsel-ddss/resources/views/stage/index.blade.php
+++ telkomsel-ddss/resources/views/stage/index.blade.php
@@ -14,7 +14,7 @@
     </div>
     <div class="px-2 py-4 my-4 border-2 rounded-lg shadow-lg sm:px-6 sm:py-8 border-sekunder">
         <span class="text-2xl font-bold sm:text-4xl text-sekunder">Form Pendaftaran</span>
-        <form action="{{ route('the_stage.store') }}" method="post">
+        <form action="{{ route('the_stage.store') }}" method="post" x-data="{modal:false}">
             @csrf
             <div class="grid grid-cols-3 gap-6 mt-4 md:grid-cols-3">
                 <div class="w-full col-span-full md:col-span-1">
@@ -90,7 +90,23 @@
                     <input class="mr-2 border-2 rounded outline-2 outline-sekunder ring-sekunder border-sekunder" type="checkbox" name="agree" id="agree" required>
                     <label for="agree" class="font-semibold text-sekunder">Saya menyetujui bahwa video saya akan ditayangkan di khalayak umum</label>
                 </div>
-                <button type="submit" class="col-span-3 px-4 py-2 font-bold text-white uppercase transition-all border-2 rounded bg-sekunder hover:text-sekunder border-sekunder hover:bg-white ">Daftar</button>
+                {{-- <button type="submit" class="col-span-3 px-4 py-2 font-bold text-white uppercase transition-all border-2 rounded bg-sekunder hover:text-sekunder border-sekunder hover:bg-white ">Daftar</button> --}}
+                <span class="col-span-3 px-4 py-2 font-bold text-center text-white uppercase transition-all border-2 rounded cursor-pointer bg-sekunder hover:text-sekunder border-sekunder hover:bg-white" x-on:click="modal=!modal">Daftar</sp>
+            </div>
+
+            <div class="fixed inset-0 z-20 flex items-center justify-center w-full h-full overflow-auto bg-black/80" style="display:none;" id="modal" x-show="modal" x-transition>
+                <i class="absolute z-10 text-3xl text-white transition cursor-pointer fa-solid fa-xmark top-5 right-10 hover:text-premier" x-on:click="modal=false"></i>
+                <div class="flex flex-col w-full p-4 mx-4 overflow-hidden bg-white rounded-lg sm:w-1/2">
+                    <p class="text-3xl font-extrabold text-center border-b-2 text-premier">PASTIKAN</p>
+                    <div class="p-6">
+                        <ul class="list-disc gap-x-2">
+                            <li class="font-semibold">Video tidak di upload sebelum tanggal 01 Agustus 2022.</li>
+                            <li class="font-semibold">Durasi Video maksimal 6 menit.</li>
+                            <li class="font-semibold">Video di rekam dengan orientasi landscape.</li>
+                        </ul>
+                    </div>
+                    <button type="submit" class="w-full col-span-3 px-4 py-2 mt-6 font-bold text-white uppercase transition-all border-2 rounded bg-sekunder hover:text-sekunder border-sekunder hover:bg-white ">Daftar</button>
+                </div>
             </div>
         </form>
     </div>
