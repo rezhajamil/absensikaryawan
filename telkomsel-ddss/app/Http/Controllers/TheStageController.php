cadccd6724e9ab48350930b98bf6b4a7bf25fff3
John Doe <yamatogija@gmail.com>
2023-02-20 15:39:22 +0700

    New Event

--- telkomsel-ddss/app/Http/Controllers/TheStageController.php
+++ telkomsel-ddss/app/Http/Controllers/TheStageController.php
@@ -36,6 +36,7 @@ class TheStageController extends Controller
      */
     public function store(Request $request)
     {
+        $event = DB::table('list_event')->where('status', 1)->first();
         $request->validate([
             'npsn' => 'required|numeric|digits:8',
             'nama' => 'required',
@@ -45,13 +46,13 @@ class TheStageController extends Controller
             'jenis' => 'required',
             'agree' => 'required',
             'alasan' => 'required',
-            'telp' => 'required|numeric|digits_between:11,13',
+            // 'telp' => 'required|numeric|digits_between:11,13',
             'telp' => ['required', 'numeric', 'digits_between:11,13', new TelkomselNumber],
             'wa' => 'required|numeric|digits_between:11,13',
             'url' => 'required|url',
         ]);
 
-        $count = DB::table('peserta_event')->where('telp', $request->telp)->where('kategori', 'The Stage')->count();
+        $count = DB::table('peserta_event')->where('telp', $request->telp)->where('kategori', 'The Stage')->where('event', $event->id)->count();
 
         if ($count > 0) {
             return redirect('/')->with('error', 'Anda Sudah Mendaftar di The Stage');
@@ -68,7 +69,7 @@ class TheStageController extends Controller
                 'alasan' => $request->alasan,
                 'youtube' => $request->url,
                 'kategori' => 'The Stage',
-                'event' => 2,
+                'event' => $event->id,
             ]);
 
             return redirect('/')->with('success', 'Anda Berhasil Mendaftar di The Stage');
