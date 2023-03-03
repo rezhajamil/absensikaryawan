bd37144714f9420bf6eae1f1269532aa3d6297fa
Rezha Jamil <yamatogija@gmail.com>
2023-03-03 22:09:15 +0700

    Delete telkomsel-ddss directory

--- telkomsel-ddss/resources/views/components/navbar.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="flex items-center justify-between w-full px-4 py-6 shadow-md bg-premier shadow-tersier">
-    {{-- <a class="flex items-center h-10 md:h-16" href="{{ URL::to('/') }}">
-        <img src="{{ asset('images/logo-gubernur.svg') }}" alt="Pemkot Medan" class="z-10 h-full">
-    </a> --}}
-    <a class="flex items-center h-10 md:h-16" href="{{ URL::to('/') }}">
-        <img src="{{ asset('images/white.png') }}" alt="Telkomsel" class="z-10 h-3/4">
-    </a>
-    <span class="inline-block w-1/2 text-xs font-bold text-center text-white sm:w-full md:text-3xl sm:text-xl font-batik sm:absolute selection:bg-white selection:text-premier">PARADE KAMPUS DAN SEKOLAH SUMATERA</span>
-</div>
